21fab65cb6edfd2a902f36df83b9319a2bb1082c
Flora Jin <jinflora2004@gmail.com>
2026-02-01 16:28:39 -0800

    Comment changes

--- speech-to-text.py
+++ speech-to-text.py
@@ -5,21 +5,19 @@ import whisper
 import numpy as np
 import time
 
-# 1. SSL Context Fix
+# SSL Content fix (was required on personal laptop - may not be needed in ubuntu 22.04 alienware)
 ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
-# 2. Load Model
+# Load Whisper Model
 print("⏳ Loading Whisper Model...")
 model = whisper.load_model("base")
 recognizer = sr.Recognizer()
 
-# --- CONFIGURATION ---
-MICROPHONE_INDEX = 1  # Change this to the index you found!
-# ---------------------
+# Index for selecting desired microphone
+MICROPHONE_INDEX = 1  # Change this to the index found
 
-# 3. Initialize Mic with 16kHz Sample Rate
-# Whisper requires 16000Hz. If we don't set this, it defaults to 44100/48000
-# and the audio sounds like slow-motion to the model.
+# Initialize Mic with 16kHz Sample Rate
+# Whisper requires 16000Hz. If we don't set this, it defaults to 44100/48000 which would make audio sound slow motion
 mic = sr.Microphone(device_index=MICROPHONE_INDEX, sample_rate=16000)
 
 print("🎤 Listening...")
@@ -36,8 +34,7 @@ with mic as source:
             print("Listening for phrase...")
             audio = recognizer.listen(source, phrase_time_limit=3)
 
-            # Convert raw bytes (16-bit int) to NumPy float32
-            # because we set sample_rate=16000 above, this data is already 16k
+            # Convert raw bytes (16-bit int) to NumPy float32 because we set sample_rate=16000 above, this data is already 16k
             audio_data = audio.get_raw_data()
             
             audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
@@ -59,7 +56,6 @@ with mic as source:
                 print("🛑 STOP COMMAND RECOGNIZED")
                 last_cmd_time = time.time()
                 # You might want to break the loop here if you actually want to quit
-                # break 
 
         except sr.WaitTimeoutError:
             pass # No speech detected, ignore
